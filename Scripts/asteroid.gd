557a7052ac5f1cdbf2f216e59d175cf752859220
Ben Schwabe <bspymaster@gmail.com>
2024-04-11 21:33:27 -0500

    Add item drop ability to asteroids

--- Scripts/asteroid.gd
+++ Scripts/asteroid.gd
@@ -1,7 +1,9 @@
 extends InteractiveEntity
 
-@export var max_rotation_velocity = 5
-@export var point_value = 1
+@export var max_rotation_velocity: int = 5
+@export var point_value: int = 1
+@export_range(0,1) var drop_item_chance: float = 1
+@export var upgrade_pickup_scene: PackedScene
 
 
 func _ready():
@@ -17,3 +19,21 @@ func _on_body_entered(body):
 func take_damage():
 	super.take_damage()
 	get_tree().get_current_scene().add_points(point_value)
+	if (_should_drop_item()):
+		_spawn_scene(upgrade_pickup_scene)
+
+
+func _should_drop_item():
+	var rand_num = randf()
+	if rand_num < drop_item_chance:
+		return true
+	return false
+
+
+func _spawn_scene(scene: PackedScene):
+	var instance = scene.instantiate()
+	instance.position = position
+	instance.rotation = rotation
+	instance.linear_velocity = linear_velocity
+	
+	add_sibling(instance)
